1c0b2cf71134062fb3ec63107714c7007fb8709b
Soros87 <timkwan87@gmail.com>
2023-11-23 15:58:25 +0800

    create login page

---
+++ client/src/assets/index.js
@@ -0,0 +1,3 @@
+import NoProfile from "./userprofile.png";
+import BgImg from "./img.jpeg";
+export { NoProfile, BgImg };
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { TbSocial } from "react-icons/tb";
+import { TextInput, CustomButton } from "../components";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { BgImg } from "../assets";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+  const [errMsg, setErrMsg] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const dispatch = useDispatch();
+
   return (
     <div className="bg-bgColor w-full h-[100vh] flex items-center justify-center p-6">
       <div className="w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl">
         {/* Left */}
-        <div className="w-full lg:w=1/2 h-full p-10 2xl:px-20 flex flex-col justify-center">
+        <div className="w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center">
           <div className="w-full flex gap-2 items-center mb-6">
             <div className="p-2 bg-[#065ad8] rounded text-white">
               <TbSocial />
@@ -19,9 +34,77 @@ const Login = () => {
             Log in to your account
           </p>
           <span className="text-sm mt-2 text-ascent-2">Welcome back</span>
-          <form className="py-8 flex flex-col gap-5"></form>
+          <form className="py-8 flex flex-col gap-5">
+            {" "}
+            <TextInput
+              name="email"
+              label="Email Address"
+              placeholder="email@example.com"
+              type="email"
+              register={register("email", {
+                required: "Email Address is required!",
+              })}
+              styles="w-full rounded-full"
+              labelStyle="ml-2"
+              error={errors.email ? errors.email.message : ""}
+            />
+            <TextInput
+              name="password"
+              label="Password"
+              placeholder="Password"
+              type="password"
+              styles="w-full rounded-full"
+              labelStyle="ml-2"
+              register={register("password", {
+                required: "Password is required!",
+              })}
+              error={errors.password ? errors.password?.message : ""}
+            />
+            <Link
+              to="/reset-password"
+              className="text-sm text-right text-blue font-semibold"
+            >
+              Forgot Password ?
+            </Link>
+            {errMsg?.message && (
+              <span
+                className={`text-sm ${
+                  errMsg?.status === "failed" ? "text-failure" : "text-success"
+                } mt-0.5`}
+              >
+                {errMsg?.message}
+              </span>
+            )}
+            {isSubmitting ? (
+              ""
+            ) : (
+              <CustomButton
+                type="submit"
+                containerStyles={`inline-flex justify-center rounded bg-blue px-8 py-3 text-sm font-medium text-white outline-none`}
+                title="Login"
+              />
+            )}
+          </form>
+          <p className="text-ascent-2 text-sm text-center">
+            Don't have an account
+          </p>
+          <Link
+            to="/register"
+            className="text-sm text-center text-blue font-semibold"
+          >
+            Sign up
+          </Link>
         </div>
         {/* Right */}
+        <div className="hidden w-1/2 full lg:flex flex-col items-center justify-center bg-blue">
+          <div className="relative w-full flex items-center justify-center">
+            <img
+              src={BgImg}
+              alt="bg"
+              className="w-48 2xl:w-64 h-48 2xl:h-64 rounded-full object-cover"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
