3ed9ca8fc7c940eeee0243b0145e2c20b8d43aae
Soros87 <timkwan87@gmail.com>
2023-12-04 10:42:26 +0800

    Add post route to index.js

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import userAuth from "../middleware/authMiddleware.js";
+import { createPost } from "../controllers/postController.js";
+
+const router = express.Router();
+
+//create post
+router.post("/create-post", userAuth, createPost);
+
+export default router;
